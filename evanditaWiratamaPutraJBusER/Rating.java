851c89e83c5b3a76ad7cdba9d1d10f9e5dfb1d6a
Evandita <evanditaw@gmail.com>
2023-09-14 22:52:31 +0700

    CS02 - menyelesaikan PreCS

---
+++ evanditaWiratamaPutraJBusER/Rating.java
@@ -0,0 +1,13 @@
+package evanditaWiratamaPutraJBusER;
+
+public class Rating
+{
+    private long count;
+    private long total;
+
+    public Rating()
+    {
+        this.total = 0;
+        this.count = 0;
+    }
+}
